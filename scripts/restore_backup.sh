dce5da302c82cdb248251d790b1f88489064d685
Linton Fisher <18075646+lintonfisher@users.noreply.github.com>
2019-08-23 09:42:20 +0100

    Formatting changes

--- scripts/restore_backup.sh
+++ scripts/restore_backup.sh
@@ -11,19 +11,23 @@ then
 fi
 
 # Restore backups
-if [ -f $backup_dir/.bashrc_backup ]; then
+if [ -f $backup_dir/.bashrc_backup ]
+then
 	rm $HOME/.bashrc
 	mv $backup_dir/.bashrc_backup $HOME/.bashrc
 fi
-if [ -f $backup_dir/.bash_aliases_backup ]; then
+if [ -f $backup_dir/.bash_aliases_backup ]
+then
 	rm $HOME/.bash_aliases
 	mv $backup_dir/.bash_aliases_backup $HOME/.bash_aliases
 fi
-if [ -f $backup_dir/.dircolors_backup ]; then
+if [ -f $backup_dir/.dircolors_backup ]
+then
 	rm $HOME/.dircolors
 	mv $backup_dir/.dircolors_backup $HOME/.dircolors
 fi
-if [ -f $backup_dir/.inputrc_backup ]; then
+if [ -f $backup_dir/.inputrc_backup ]
+then
 	rm $HOME/.inputrc
 	mv $backup_dir/.inputrc_backup $HOME/.inputrc
 fi
